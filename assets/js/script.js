c87efa68b43e442546ad954ac9f7bacb18eade85
Gabriela <glabradadev@gmail.com>
2026-05-11 17:17:38 -0300

    feat: Criação da parte 'personagens' e uma função js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+function transformarDarth(){
+    document.getElementById("img-anakin").src = 'assets/img/darth-vader.jpg'
+        document.getElementById('nome-anakin').innerText = 'Darth Vader'
+}
